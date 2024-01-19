9618440218ffaad777ec0196de88a5a9af0178a6
ViktoriaAstashonok <122493150+ViktoriaAstashonok@users.noreply.github.com>
2024-01-19 13:42:23 +0300

    fixes

--- app/src/main/java/com/example/flagsapp/MainActivity.kt
+++ app/src/main/java/com/example/flagsapp/MainActivity.kt
@@ -1,54 +1,47 @@
 package com.example.flagsapp
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.flagsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var countries: ArrayList<Country>
-    private lateinit var flags: Array<Int>
-    private lateinit var names: Array<String>
+    private val recyclerView: RecyclerView by lazy { findViewById(R.id.recyclerView) }
+    private val countries: ArrayList<Country> by lazy { arrayListOf() }
+    private val flags: Array<Int> by lazy { getCountryFlagIds() }
+    private lateinit var names: List<String>
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        val resources = applicationContext.resources
         setContentView(R.layout.activity_main)
 
-        flags = arrayOf(
-            R.drawable.flag_of_austria_svg,
-            R.drawable.flag_of_poland_svg,
-            R.drawable.flag_of_italy_svg,
-            R.drawable.flag_of_colombia_svg,
-            R.drawable.flag_of_madagascar_svg,
-            R.drawable.flag_of_thailand_svg,
-            R.drawable.flag_of_denmark_svg,
-            R.drawable.flag_of_switzerland__pantone__svg
-        )
+        names = getCountryNames(resources)
 
-        names = arrayOf(
-            "Austria",
-            "Poland",
-            "Italy",
-            "Columbia",
-            "Madagascar",
-            "Thailand",
-            "Denmark",
-            "Switzerland"
-        )
-
-        recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
 
-        countries = arrayListOf<Country>()
         getCountries()
     }
+
+    private fun getCountryFlagIds() = arrayOf(
+        R.drawable.flag_of_austria_svg,
+        R.drawable.flag_of_poland_svg,
+        R.drawable.flag_of_italy_svg,
+        R.drawable.flag_of_colombia_svg,
+        R.drawable.flag_of_madagascar_svg,
+        R.drawable.flag_of_thailand_svg,
+        R.drawable.flag_of_denmark_svg,
+        R.drawable.flag_of_switzerland__pantone__svg
+    )
+
+    private fun getCountryNames(resources: Resources): List<String> =
+        resources.getStringArray(R.array.country_names).toList()
 
     private fun getCountries() {
         for (i in flags.indices) {
@@ -57,4 +50,6 @@ class MainActivity : AppCompatActivity() {
         }
         recyclerView.adapter = CountryAdapter(countries)
     }
+
+
 }
